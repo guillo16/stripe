450ec5565cfc5039ee12edafb13ea91b089bdd5c
guillermo varela <guillovarela16@gmail.com>
2020-06-22 11:21:41 -0300

    Create controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
+  get 'categories/index'
+  get 'categories/show'
+  get 'categories/new'
+  get 'categories/create'
+  get 'categories/edit'
+  get 'categories/update'
+  get 'categories/destroy'
   devise_for :users
-  root to: 'pages#home'
+  root to: 'category#index'
+  resources :categories
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
