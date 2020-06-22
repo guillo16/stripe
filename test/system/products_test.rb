c2f11f136f686af22a8b45d6ca0d2b6a67968d3b
guillermo varela <guillovarela16@gmail.com>
2020-06-22 15:12:22 -0300

    Finish the test

--- test/system/products_test.rb
+++ test/system/products_test.rb
@@ -1,14 +1,19 @@
 require "application_system_test_case"
 
 class ProductsTest < ApplicationSystemTestCase
+  setup do
+    @category = categories(:backpack)
+  end
  test "lets a signed in user create a new product_title" do
   login_as users(:george)
   visit "/products/new"
     # save_and_open_screenshot
+    puts @category
 
     fill_in "product_title", with: "Le Wagon"
     fill_in "product_photo", with: "Le Wagon"
     fill_in "product_description", with: 'Something'
+    fill_in 'category_id', with: @category
 
     click_on 'Create Product'
 
