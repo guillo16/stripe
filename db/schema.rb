9b307021e505f52d0195f178783c05664e9a7716
guillermo varela <guillovarela16@gmail.com>
2020-06-24 17:41:11 -0300

    Add new fields

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_22_145633) do
+ActiveRecord::Schema.define(version: 2020_06_24_204033) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,8 @@ ActiveRecord::Schema.define(version: 2020_06_22_145633) do
     t.bigint "category_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "color"
+    t.integer "stock"
     t.index ["category_id"], name: "index_products_on_category_id"
   end
 
