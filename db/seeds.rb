8f8c0607258166458cf6b85c99094808599965c6
guillermo varela <guillovarela16@gmail.com>
2020-06-24 17:59:06 -0300

    Add faker

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+puts 'clearing database'
+
+Product.delete_all
+
+10.times do
+  product.create
